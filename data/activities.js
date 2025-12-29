8b255fc75562a98ca6aebea0d0ca16d7cd5c19aa
Daniel Onís Fabián <danielonisfabian@MacBook-Air-de-Daniel-244.local>
2025-12-29 16:29:37 +0100

    form made

--- data/activities.js
+++ data/activities.js
@@ -1,7 +1,7 @@
 let activities = [
     { name:"UNICEF", money:0 },
     { name:"Save the children", money:0 },
-    { name:"WWT", money:0 }
+    { name:"WWF", money:0 }
 ]
 
 module.exports = activities
--- routers/routerActivities.js
+++ routers/routerActivities.js
@@ -2,11 +2,42 @@ const express = require("express")
 
 const routerActivities = express.Router()
 let activities = require("../data/activities")
+let users = require("../data/users")
 
 routerActivities.get("/", (req,res) => {
     res.render("index", {
         activities: activities
     })
+})
+
+routerActivities.post("/donate", (req,res) => {
+    let userName = req.body.userName
+    let password = req.body.password
+    let activityName = req.body.activityName
+    let money = parseFloat(req.body.money)
+
+    //check
+    let user = users.find( u => u.name == userName && u.password == password )
+    if(user == null) {
+        res.send("User not find")
+        return
+    }
+
+    if(user.money <= money) {
+        res.send("Not enough money")
+        return
+    }
+
+    let activity = activities.find( a => a.name == activityName)
+    if(activity == null) {
+        res.send("Unexistent activity")
+        return
+    }
+    
+    user.money = user.money - money
+    activity.money = activity.money + money
+    res.redirect("/activities")
+
 })
 
 module.exports = routerActivities
